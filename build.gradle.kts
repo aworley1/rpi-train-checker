463436193366a1cf26b00bd2b999f75dd3947dfd
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-08-11 12:39:08 +0100

    Import Darwin WSDL and fix Gradle dependencies
    WSDL imported using:
    wsimport -verbose -XadditionalHeaders -keep https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,18 +21,16 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    implementation("org.http4k:http4k-core:$http4kVersion")
-    implementation("org.http4k:http4k-client-okhttp:$http4kVersion")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
+    implementation("org.glassfish.jaxb:txw2:2.3.2")
+    implementation("com.sun.xml.ws:jaxws-rt:2.3.2")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.17")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
-    testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.http4k:http4k-server-jetty:$http4kVersion")
     testImplementation(gradleTestKit())
 }
 
