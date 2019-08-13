5f7d9ebf5fec144554198f4ab107515c76f117ae
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-08-13 21:20:01 +0100

    Only get trains if any requested in next hour

--- src/main/kotlin/io/github/aworley1/rpi_train_checker/ProblemDeterminer.kt
+++ src/main/kotlin/io/github/aworley1/rpi_train_checker/ProblemDeterminer.kt
@@ -1,13 +1,29 @@
 package io.github.aworley1.rpi_train_checker
 
+import java.time.Clock
+import java.time.Duration
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
 typealias ProblemDeterminer = (times: List<String>) -> Unit
 
 fun createProblemDeterminer(
         departureStation: String,
         destinationStation: String,
-        getTrains: GetTrains
+        getTrains: GetTrains,
+        clock: Clock
 ): ProblemDeterminer {
-    return {
-        getTrains(departureStation, destinationStation)
+    return {requestedTrains ->
+        val currentTime = ZonedDateTime.ofInstant(clock.instant(), ZoneId.of("Europe/London")).toLocalTime()
+
+        val requestedTimes = requestedTrains
+                .map { LocalTime.parse(it) }
+
+        if (requestedTimes.any { withinThreshold(it, currentTime) }) {
+            getTrains(departureStation, destinationStation)
+        }
     }
 }
+
+private fun withinThreshold(time: LocalTime, currentTime: LocalTime) = Duration.between(currentTime, time).toMinutes() <= 60L
--- src/test/kotlin/io/github/aworley1/rpi_train_checker/ProblemDeterminerTest.kt
+++ src/test/kotlin/io/github/aworley1/rpi_train_checker/ProblemDeterminerTest.kt
@@ -2,17 +2,27 @@ package io.github.aworley1.rpi_train_checker
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+
+val ELEVEN_AM_CLOCK = Clock.fixed(
+        Instant.parse("2015-01-01T11:00:00Z"),
+        ZoneId.of("Europe/London")
+)
 
 object ProblemDeterminerTest : Spek({
-    describe("Given there are trains within the next hour") {
+    describe("Given there are trains requested within the next hour") {
         val times = listOf("11:23")
         val mockGetTrains = MockGetTrains()
         val problemDeterminer = createProblemDeterminer(
                 "departure-station",
                 "destination-station",
-                mockGetTrains.mockGetTrains
+                mockGetTrains.mockGetTrains,
+                ELEVEN_AM_CLOCK
         )
 
         problemDeterminer(times)
@@ -25,6 +35,24 @@ object ProblemDeterminerTest : Spek({
         it("should call getTrains with correct destination station") {
             assertThat(mockGetTrains.wasCalledWith?.destinationStation)
                     .isEqualTo("destination-station")
+        }
+    }
+
+    describe("Given there are NO trains requested within the next hour") {
+        val times = listOf("15:00")
+        val mockGetTrains = MockGetTrains()
+        val problemDeterminer = createProblemDeterminer(
+                "departure-station",
+                "destination-station",
+                mockGetTrains.mockGetTrains,
+                ELEVEN_AM_CLOCK
+        )
+
+        problemDeterminer(times)
+
+        it("should NOT call GetTrains") {
+            assertThat(mockGetTrains.wasCalledWith)
+                    .isNull()
         }
     }
 })
