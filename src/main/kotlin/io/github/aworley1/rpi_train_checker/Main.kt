db65b4df37d60baad9cc16772878599b6462341f
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-08-22 23:05:20 +0100

    Configure pi4j to use /dev/gpiomem
    so that this doesn't need to run as root

--- src/main/kotlin/io/github/aworley1/rpi_train_checker/Main.kt
+++ src/main/kotlin/io/github/aworley1/rpi_train_checker/Main.kt
@@ -1,6 +1,7 @@
 package io.github.aworley1.rpi_train_checker
 
 import com.pi4j.io.gpio.GpioFactory
+import com.pi4j.wiringpi.GpioUtil
 import com.thalesgroup.rtti._2013_11_28.token.types.AccessToken
 import com.thalesgroup.rtti._2017_10_01.ldb.Ldb
 import java.time.Clock
@@ -9,6 +10,8 @@ import java.time.ZoneId
 val TIMEZONE = ZoneId.of("Europe/London")
 
 fun main() {
+    GpioUtil.enableNonPrivilegedAccess()
+
     val requestedTimes = System.getenv("TRAINS_REQUESTED_TIMES").split(",")
     val departureStation = System.getenv("TRAINS_DEPARTURE_STATION")
     val destinationStation = System.getenv("TRAINS_DESTINATION_STATION")
