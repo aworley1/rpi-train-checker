63a16652d778385eac8f5a1c1d4cb8a9904c0772
Andrew Worley <andrew.worley@johnlewis.co.uk>
2019-08-23 21:04:32 +0100

    Special build which sets LEDs high regardless

--- src/main/kotlin/io/github/aworley1/rpi_train_checker/StatusReporter.kt
+++ src/main/kotlin/io/github/aworley1/rpi_train_checker/StatusReporter.kt
@@ -3,9 +3,6 @@ package io.github.aworley1.rpi_train_checker
 import com.pi4j.io.gpio.GpioController
 import com.pi4j.io.gpio.PinState
 import com.pi4j.io.gpio.RaspiPin
-import io.github.aworley1.rpi_train_checker.ProblemStatus.NO_PROBLEM
-import io.github.aworley1.rpi_train_checker.ProblemStatus.NO_TRAINS_REQUESTED
-import io.github.aworley1.rpi_train_checker.ProblemStatus.PROBLEM
 
 val GREEN_LED_PIN = RaspiPin.GPIO_02
 val RED_LED_PIN = RaspiPin.GPIO_04
@@ -17,10 +14,7 @@ fun createStatusReporter(gpioController: GpioController): StatusReporter {
         val greenLed = gpioController.provisionDigitalOutputPin(GREEN_LED_PIN, PinState.LOW)
         val redLed = gpioController.provisionDigitalOutputPin(RED_LED_PIN, PinState.LOW)
 
-        when (problemStatus) {
-            PROBLEM -> redLed.high()
-            NO_PROBLEM -> greenLed.high()
-            NO_TRAINS_REQUESTED -> {}
-        }
+        redLed.high()
+        greenLed.high()
     }
 }
